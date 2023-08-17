a294ea56d1b3f204790bf41a4fcd5b70345fce6e
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-08-17 12:02:07 -0400

    a-few-css-updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,7 +10,11 @@ export default function Cart() {
   const [showPopup, setShowPopup] = useState(false); 
   const { cartItemsCount, updateCartItemsCount } = useCartItemsCount();// Use the custom hook
 
- 
+  function generateRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 100000000); // Generates a random number between 0 and 99999999
+    const paddedNumber = randomNumber.toString().padStart(8, '0'); // Pads the number with leading zeros if it's less than 8 digits
+    return paddedNumber;
+  }
  
   const handleIncrease = (item) => {
     increaseQt(item);
@@ -55,10 +59,13 @@ export default function Cart() {
       <h1 className="cartTitle">Cart</h1>{cart.length === 0 && <p>No items in cart.</p>}
       {showPopup && (
         <div className="popup-container">
-         <p>Your total is: &#9763;{cart.reduce((acc, item) => acc + item.priceScrap * item.quantity, 0)}</p>
+            <h2>Order Recived</h2>
+         <p>Your total was: &#9763;{cart.reduce((acc, item) => acc + item.priceScrap * item.quantity, 0)}</p>
          
-          <p>Your designated drones will arrive shortly.</p>
-          <button onClick={() => setShowPopup(false)}>Close</button>
+          <p>Your designated drones will arrive shortly!</p>
+<div className="tracking"> <h4>tracking number:</h4><p>{generateRandomNumber()}</p></div>
+         
+          <button className="close" onClick={() => setShowPopup(false)}>Close</button>
         </div>
       )}
       <div className="itemsArea"> <div className="items">
@@ -88,7 +95,7 @@ export default function Cart() {
                   -
                 </button>
               </div>
-              <button onClick={() => handleRemove(item)} className="remove">
+              <button className="remove" onClick={() => handleRemove(item)} >
                 Remove
               </button>
             </div>
